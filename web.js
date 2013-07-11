916b85ab74e00b0e0523e24616363c4944a65647
Vikas Bansal <vikasapr@gmail.com>
2013-07-11 07:09:45 +0000

    Updated5 web.js for reading from index.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
   var fs = require('fs');
-  var buf = new buffer(fs.readFileSync('index.html'), 'utf-8');
+  var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
 //  response.send('Hello World 2!');
   response.send(buf.toString());
 });
